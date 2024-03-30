df0a74674b5abe886e752a0c60a2b4dca64cd055
Engincan Gündüz <103529823+engincangunduz@users.noreply.github.com>
2024-03-30 18:55:31 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Engin Gündüz on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
